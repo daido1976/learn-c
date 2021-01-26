9831c7a138809fcf40ccd7e0bdedccbcc94a44db
Daido Shota <daido1976@gmail.com>
2021-01-26 10:13:19 +0900

    Add cpp directory & hello world

---
+++ cpp/hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << to_string(1 + 1) + "\n";
+  cout << "Hello world!\n";
+  cout << "Hello world!" << endl;
+
+  printf("Hello world!\n");
+}
